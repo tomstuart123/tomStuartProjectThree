f21a96e390876d8e50e55144b70b63a8ad18c165
Tommy <tomstuart123@hotmail.co.uk>
2019-11-10 09:41:00 -0500

    first commit - landing & guest started

---
+++ index.js
@@ -0,0 +1,22 @@
+// LANDING PAGE
+    // Am I a host or guest 
+// HOST
+    //PAGE 1
+        // Quiz (where do you live, day you want yoga session, number of attendees, favourite type of yoga)
+        // Explore yoga options
+        // NAV - back to guest or Host
+    // Page 2
+        // Options offered and shown depend on what parameters they prefer
+        // filter buttons at the top
+        // Also button to go back to the form 
+
+// GUEST
+    // Page 1
+        // Quiz (where do you want to host, where is this, how many people can you take, type of yoga, your experience level) >> 
+        // Explore what others are doing >> PUSH TO HOST SITE
+    // Page 2
+        //  Profile review
+        //  submit button
+    // Page 3
+        // Add profile to the profile options
+
