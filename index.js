6b33aa7f5c089bc80b42db3f48edad01a09ca761
Tommy <tomstuart123@hotmail.co.uk>
2019-11-13 09:03:04 -0500

    able to append to page something

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// SITE STRUCTURE
+// Long-Term SITE STRUCTURE
 
     // LANDING PAGE (MVP)
-        // Am I a host or guest 
-    // HOST (MVP)
+        // A landing page
+        // Base Description
+        // Two buttons driving tow host or guest
+    // Guest (MVP)
         //PAGE 1
             // Quiz (where do you live, day you want yoga session, number of attendees, favourite type of yoga)
             // Explore yoga options
@@ -13,7 +15,7 @@
             // Also button to go back to the form 
 
     
-    // GUEST (STRETCH-GOAL)
+    // Host (STRETCH-GOAL)
         // Page 1
             // Quiz (where do you want to host, where is this, how many people can you take, type of yoga, your experience level) >> 
             // Explore what others are doing >> PUSH TO HOST SITE
@@ -26,28 +28,24 @@
 // PSEUDO CODE
 
     // MVP - PSEUDO CODE
-    // On landingPage.html, show user buttons of Am I host or a guest yogi 
-    // on click of host ->>> get sent to coming soon page
-    // on click of guest ->>> get sent to guest.html
-
-
-    // guest.html shows a form with how they want their yoga class (e.g. city, day of the week, level of experience, type of yoga etc.)
-    // on submit button of form, i) check the checked fields in the form and ii) send the user to explore.html
-            // in backend there is an object of each yoga class
-            // on click event prevent default
+    // guest.html shows a form with how they want their yoga class (e.g. level of experience and atmosphere) (STRETCH GOAL - add more filter options e.g. day of the week)
+    // on submit button of form, i) check the checked fields in the form and ii) append the relevant class options to the page.
+    // HOW
+            // I have created javascript objects of each yoga class that matches the items in the form field. They also have a name of the Yoga teacher and a visual
+            // on click of submit event .preventdefault()
             // then find the checked answer to each question in the form using ':checked'
             // run through the object vs. these ':checked' responses using .filter()
-            // .append() only the objects that meet the correct criteria to explore.html
-            // send the user to explore.html
-    // in explore html user can click into each option and they will get a coming soon message
+            // .append() only the objects that meet the correct criteria to the page below it (or STRETCH GOAL - send them another html site)
+            // finally, use an automatic scroll down feature so the user can view their options easily
 
     // STRETCH GOAL 1.1 - PSEUDO-CODE
     // on explore.html, if user clicks on a class, user isn't sent to a coming soon
     // instead user is sent to a standard profile page html. Depending on which class is clicked, loop through the corresponding JS object holding that class. Populate the profile page with the class data from the object using .text(), .html() etc. 
 
     // STRETCH GOAL 1.2 - PSEUDO-CODE
-    // on landingPage.html, if user clicks host don't send them to coming soon page
-    // send the user to host.html. This is very similar to guest.html and its form. However, added questions that those hosting a yoga class would need to answer (e.g. name, email etc).
+    // Create landingPage.html, show user buttons of Am I host or a guest yogi 
+    // on click of host ->>>  send the user to host.html. 
+    // host.html This is very similar to guest.html and its form. However, added questions that those hosting a yoga class would need to answer (e.g. name, email etc).
     // on submit of the form, it checks the form using :checked and uses the data to create a javascript object for the class. It also sends the user to profilePage.html
     // On profilePage.html, it would loop throguh the javascript object and populate the profile page with data from the javascript object
 
@@ -64,80 +62,135 @@
 
 // REAL CODE
 
+let yogaApp = {};
+
 // create class for iteration of session to hold yoga sessions. Inside the class use a static function to allow easy creation of new session classes
-class Session {
-    constructor({ city, day, experience, yoga, atmosphere }) {
+yogaApp.Session = class Session {
+    constructor({ city, day, experience, yoga, atmosphere, imageURL, name}) {
         this.city = city;
         this.day = day;
         this.experience = experience;
         this.yoga = yoga;
         this.atmosphere = atmosphere;
+        this.imageURL = imageURL;
+        this.name = name
     }
 
-    static createSession({ city, day, experience, yoga, atmosphere }) {
-        return new this({ city, day, experience, yoga, atmosphere,
+    static createSession({ city, day, experience, yoga, atmosphere, imageURL, name }) {
+        return new this({
+            city, day, experience, yoga, atmosphere, imageURL, name
         })
     }
 }
 
 // created some dummy data that always exists ont he page and stored them in grouped sessions. 
 // Will also use this object to add future sessions based on host
+// 
 // Will also use this object to filter and pull data to put on the page
-const groupedSessions = {
-    tomsBeachYoga: Session.createSession({
-        city: 'Toronto',
+yogaApp.groupedSessions = {
+    yogiTom: yogaApp.Session.createSession({
+        city: 'toronto',
         day: 'Saturday',
         experience: 'Intermediate',
         yoga: 'Slow flow',
         atmosphere: 'Outdoor',
+        imageURL: 'assets/option-visuals/artem-beliaikin-nKCtkaW4JU4-unsplash.jpg',
+        name: 'yogiTom',
+
     }),
 
-    alexsKitchenYoga: Session.createSession({
-        city: 'London',
+    yogiAlex: yogaApp.Session.createSession({
+        city: 'new-york',
         day: 'Monday',
         experience: 'Beginner',
-        yoga: 'Power flow',
+        yoga: 'Power flow', 
         atmosphere: 'Outdoor',
+        imageURL: 'assets/option-visuals/jonathan-borba-5IjWRNGbkYI-unsplash.jpg',
+        name: 'yogiAlex',
+        
     }),
 
-    sofiasStudioYoga: Session.createSession({
-        city: 'London',
+    yogiSofia: yogaApp.Session.createSession({
+        city: 'london',
         day: 'Wednesday',
         experience: 'Advanced',
         yoga: 'Ashtanga',
         atmosphere: 'Studio',
+        imageURL: 'assets/option-visuals/nate-johnston-2gBpsNuHcyA-unsplash.jpg',
+        name: 'yogiSofia',
     }),
 
-    zacsHotYoga: Session.createSession({
-        city: 'New York',
+    yogiZac: yogaApp.Session.createSession({
+        city: 'new-york',
         day: 'Friday',
         experience: 'Beginner',
         yoga: 'Vinyasa',
         atmosphere: 'Studio',
+        imageURL: 'assets/option-visuals/simon-raeker.jpg',
+        name: 'yogiZac',
     }),
 
+    yogiSandy: yogaApp.Session.createSession({
+        city: 'toronto',
+        day: 'Thursday',
+        experience: 'Intermediate',
+        yoga: 'Ashtanga',
+        atmosphere: 'Studio',
+        imageURL: 'assets/option-visuals/nate-johnston-2gBpsNuHcyA-unsplash.jpg',
+        name: 'yogiSandy',
+    }),
+
+    yogiJames: yogaApp.Session.createSession({
+        city: 'toronto',
+        day: 'Saturday',
+        experience: 'Advanced',
+        yoga: 'Slow flow',
+        atmosphere: 'Outdoor',
+        imageURL: 'assets/option-visuals/simon-raeker.jpg',
+        name: 'yogiJames',
+    }),
 }
 
-const tomsBeachYoga = Session.createSession({
-    city: 'Toronto',
-    day: 'Saturday',
-    experience: 'Intermediate',
-    yoga: 'Slow flow',
-    atmosphere: 'Outdoor',
-})
+console.log(yogaApp.groupedSessions.yogiTom['atmosphere']);
+console.log(yogaApp.groupedSessions.yogiZac['yoga']);
+console.log(yogaApp.groupedSessions.yogiSofia['experience']);
+console.log(yogaApp.groupedSessions.yogiAlex['imageURL']);
 
-console.log(groupedSessions.tomsBeachYoga['atmosphere']);
-console.log(groupedSessions.zacsHotYoga['yoga']);
-console.log(groupedSessions.sofiasStudioYoga['experience']);
-console.log(groupedSessions.alexsKitchenYoga['city']);
+// function:
+    // run through the object with a for loop
+    // for each city in the object, run .filter(array[array]) 
+yogaApp.appendToPage = function(array1) {
+    // variable to collate all similar items
+    let sameItems = [];
+    // loop through the object
+    for (i in yogaApp.groupedSessions) {
+        
+        // loop through the array and compare array city item to object city item. Return only those that are the same
+        let sameCity = array1[0].filter(function (option) {
+            return option === yogaApp.groupedSessions[i]['city']
+        })
+        // collate similar ones in array
+        if (sameCity[0] != null) {
+            sameItems.push(sameCity[0]);
+        }
+        // grab objects with these similarities
+        if (array1[0].includes(yogaApp.groupedSessions[i]['city'])) {
+            // console.log(yogaApp.groupedSessions[i]['name'])
+            $('.reveal-data').append(
+                `<div class='results'>
+                    <h1> ${yogaApp.groupedSessions[i]['name']} </h1>
+                </div`)
+        } 
+        // grab objects with these similarities
+    }   
+    console.log(sameItems);  
+}
 
 
 
-
-
-// jquery set up
-$(function () {
-    // store guest
+// store guest
+yogaApp.events = function() {
+    
     let $formGuest = $('.form-guest')
     $formGuest.on('submit', function (event) {
 
@@ -150,15 +203,27 @@ $(function () {
         const $cityChoiceObject = $('input[name=city]:checked');
         const $expChoiceObject = $('input[name=experience]:checked');
         const $atmosChoiceObject = $('input[name=atmosphere]:checked');
-        
 
-        // use a function loop through each choice object to deliver the choices we want in an array
+        
+        // for the easier to access form items, collate them into one array called subChoices
+        let dayChoicesArray = [];
+        let yogaChoicesArray = [];
+        let subChoices = [];
+        dayChoicesArray.push($dayChoice);
+        yogaChoicesArray.push($yogaChoice);
+        subChoices.push(yogaChoicesArray);
+        subChoices.push(dayChoicesArray);
+
+        // set an object to collect all the harder form items (needed cleaning) user's answers
+        let allChoices = [];
+
+        // use a function loop through each harder form item choice object to deliver the choices we want in an array 
         const findChoicesItems = (choiceObj) => {
             let newArray = [];
             // push each item in the chosen object into an array
             for (i = 0; i < 10; i++) {
-                newArray.push(choiceObj[i])
-            }
+                newArray.push(choiceObj[i]);
+            };
 
             // remove all null / undefined elements in this array in case nothing is chosen
             const cleanedArray = newArray.filter(function (item) {
@@ -170,25 +235,35 @@ $(function () {
             for (i in cleanedArray) {
                 finalChoiceArray.push(cleanedArray[i].id);
             };
-            return finalChoiceArray;
+            allChoices.push(finalChoiceArray);
+
+
         }
+        
+        // collate all user choices into a final array
+        findChoicesItems($cityChoiceObject);
+        findChoicesItems($expChoiceObject);
+        findChoicesItems($atmosChoiceObject);
+        allChoices = allChoices.concat(subChoices);
 
-        // store the final choices via funciton in an array for each option. We can then filter the yoga classes based on this
-        let cityChoicesArray = findChoicesItems($cityChoiceObject);
-        let dayChoicesArray = [];
-        dayChoicesArray.push($dayChoice)
-        let expChoicesArray = findChoicesItems($expChoiceObject);
-        let yogaChoicesArray = [];
-        yogaChoicesArray.push($yogaChoice)
-        let atmosChoicesArray = findChoicesItems($atmosChoiceObject);
+        console.log(allChoices);
+        yogaApp.appendToPage(allChoices);
 
-        // console.log(cityChoicesArray)
-        // console.log(dayChoicesArray)
-        // console.log(yogaChoicesArray)
-        // console.log(expChoicesArray)
-        // console.log(atmosChoicesArray)
+        // loop through choices in the array with .filter. If true that city (example) = (example)
+        // If these choices are in the dummy data, pull that object
 
+        // console.log(yogaApp.groupedSessions.tomsBeachYoga['atmosphere']);
+         
     })
+}
+    
+yogaApp.init = function () {
+    yogaApp.events();
+}
+
+
+$(function () {
+    yogaApp.init();
 });
 
 
